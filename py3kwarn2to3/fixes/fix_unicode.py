b9c35f6354d3cce44d962281c05670d416d71749
Steven Myint <git@stevenmyint.com>
2013-05-04 19:55:09 -0700

    Skip some innocuous Unicode cases
    Skip the case where "unicode = str" is done for future compatibility.
    This is related to issue #4.

--- py3kwarn2to3/fixes/fix_unicode.py
+++ py3kwarn2to3/fixes/fix_unicode.py
@@ -5,6 +5,7 @@
 import re
 from ..pgen2 import token
 from .. import fixer_base
+from ..fixer_util import find_binding
 
 _mapping = {u"unichr" : u"chr", u"unicode" : u"str"}
 _literal_re = re.compile(r"[uU][rR]?[\'\"]")
@@ -13,7 +14,14 @@ class FixUnicode(fixer_base.BaseFix):
     BM_compatible = True
     PATTERN = "STRING | 'unicode' | 'unichr'"
 
+    def start_tree(self, tree, filename):
+        super(FixUnicode, self).start_tree(tree, filename)
+        self.skip = find_binding('unicode', tree)
+
     def transform(self, node, results):
+        if self.skip:
+            return
+
         if node.type == token.NAME:
             new = node.clone()
             new.value = _mapping[node.value]
